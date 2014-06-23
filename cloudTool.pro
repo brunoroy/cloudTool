f60250576d5238710f29468fa90f23292d47a098
Bruno Roy <bruno.o.roy@gmail.com>
2014-06-23 11:46:25 -0400

    Initial commit.

---
+++ cloudTool.pro
@@ -0,0 +1,19 @@
+QT       += core
+QT       -= gui
+
+QMAKE_CXXFLAGS += -std=gnu++11
+
+TARGET = cloudTool
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
+
+INCLUDEPATH += ../../SAT/posture-vision/src /usr/local/include/pcl-1.7 /usr/include/eigen3
+
+LIBS += -lboost_filesystem -lboost_system
+LIBS += -L/usr/local/lib -lpcl_io -lpcl_common
+
+OBJECTS_DIR = build
---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
+#include <boost/operators.hpp>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+
+#include <core/optionmanager.h>
+
+#define EMTPY_STR ""
+
+static char* renameFolder = "";
+static char* filePLY = "";
+
+static OptionEntry entries[] =
+{
+    { "rename", "r", OPTION_ARG_STRING, &renameFolder, "renames all files in a specific folder." },
+    { "convert", "c", OPTION_ARG_STRING, &filePLY, "converts binary PLY file to ASCII PLY file." }
+};
+
+void renameFiles(std::string folder)
+{
+    boost::filesystem::path directory(folder);
+    boost::filesystem::directory_iterator it(directory), eod;
+
+    uint fileSize = 0;
+    BOOST_FOREACH(boost::filesystem::path const &path, std::make_pair(it, eod))
+    {
+        if(boost::filesystem::is_regular_file(path))
+        {
+            std::string oldName = path.filename().string();
+            //std::clog << "old name: " << oldName << std::endl;
+
+            uint beginOffset = 5;
+            uint endOffset = 4;
+            uint offset = beginOffset+endOffset;
+            std::string firstPart = std::string("_").append(oldName.substr(0, 4));
+            std::string lastPart = oldName.substr(beginOffset, oldName.length()-offset);
+            std::string newName = lastPart.append(firstPart).append(".ply");
+            //std::clog << "new name: " << newName << std::endl;
+
+            std::string oldFilename = std::string(folder).append(oldName);
+            std::string outputFolder("output/");
+            std::string newFilename = outputFolder.append(newName);
+
+            std::clog << "oldFilename: " << oldFilename << std::endl;
+            std::clog << "newFilename: " << newFilename << std::endl;
+            boost::filesystem::rename(oldFilename, newFilename);
+            fileSize++;
+        }
+    }
+    std::clog << fileSize << " files renamed." << std::endl;
+}
+
+void convertFile(std::string filePLY)
+{
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+    //std::clog << "filePLY: " << filePLY << std::endl;
+    //int ioMessage = pcl::io::loadPLYFile(filePLY, cloud);
+    pcl::io::loadPLYFile(filePLY, *cloud);
+
+    /*std::string newFilePLY(filePLY);
+    newFilePLY = newFilePLY.substr(0, newFilePLY.length()-4);
+    newFilePLY.append("_ASCII.ply");
+    pcl::io::savePLYFileASCII(newFilePLY, *cloud);
+
+    std::clog << "file " << newFilePLY << " converted." << std::endl;*/
+}
+
+int main(int argc, char *argv[])
+{
+    std::shared_ptr<OptionManager> optionManager;
+
+    optionManager.reset(new OptionManager(argc, argv));
+    optionManager->setOptionContext("Simple tool that implement various data operations.");
+    optionManager->addUsage("-r <folder>");
+    optionManager->addUsage("-c <filename>");
+    optionManager->addExample("-r clouds/");
+    optionManager->addExample("-c file.ply");
+    std::vector<OptionEntry> optionEntries(entries, entries + sizeof(entries)/sizeof(entries[0]));
+    optionManager->setOptionEntries(optionEntries);
+
+    OptionParserError *error = NULL;
+    if (optionManager->parseOptions(&error))
+    {
+        if (renameFolder != EMTPY_STR)
+            renameFiles(std::string(renameFolder));
+        if (filePLY != EMTPY_STR)
+            convertFile(std::string(filePLY));
+    }
+
+    return 0;
+}
